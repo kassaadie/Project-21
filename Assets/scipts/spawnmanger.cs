87e3d9e27060e32ea681c4f850af3eabf2519a88
kassadie <kassadie.b161@stu.nebo.edu>
2019-10-24 17:49:10 -0600

    Commit

---
+++ Assets/scipts/spawnmanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{    
+   public GameObject[] animePerfab;
+    public int animaIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ =20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    
+
+    // Update is called once per frame
+    void Update() { 
+    {
+       if (Input.GetKeyDown(KeyCode.S)) { 
+	  
+		int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], new Vector3 (0,0,20)
+	    animal prefabs[animalIndex].transform.rotation);
+
+	    Instantiate(animalPrefabs[animalIndex]), spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+      }
+}
